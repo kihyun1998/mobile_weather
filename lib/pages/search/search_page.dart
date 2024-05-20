7d8beb82afded293fcdeea4e7bee79aa529fed4c
박기현 <kihyun1998@naver.com>
2024-05-21 08:30:15 +0900

    검색페이지 구현

--- lib/pages/search/search_page.dart
+++ lib/pages/search/search_page.dart
@@ -1,23 +1,72 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class SearchPage extends ConsumerStatefulWidget {
+class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _SearchPageState();
+  State<SearchPage> createState() => _SearchPageState();
 }
 
-class _SearchPageState extends ConsumerState<SearchPage> {
+class _SearchPageState extends State<SearchPage> {
+  final _formKey = GlobalKey<FormState>();
+  String? _city;
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+
+  void _sumit() {
+    setState(() {
+      autovalidateMode = AutovalidateMode.always;
+    });
+    final form = _formKey.currentState;
+    if (form != null && form.validate()) {
+      form.save();
+      Navigator.of(context).pop(_city!.trim());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Search'),
-      ),
-      body: const Center(
-        child: Text('Search'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Search'),
+        ),
+        body: Form(
+          key: _formKey,
+          autovalidateMode: autovalidateMode,
+          child: Column(
+            children: [
+              const SizedBox(height: 60),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: TextFormField(
+                  autofocus: true,
+                  style: const TextStyle(fontSize: 18),
+                  decoration: const InputDecoration(
+                    labelText: "City Name",
+                    hintText: "more than 2 characters",
+                    prefixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (String? value) {
+                    if (value == null || value.trim().length < 2) {
+                      return "City name must be at least 2 characters long";
+                    }
+                    return null;
+                  },
+                  onSaved: (String? newValue) {
+                    _city = newValue;
+                  },
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _sumit,
+                child: const Text(
+                  "How's weather?",
+                  style: TextStyle(fontSize: 20),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
